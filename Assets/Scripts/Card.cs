b55d6bbd99bc25b97275af672c77dc4f8a6b3272
JoeFrumenti <111709603+jfrument@users.noreply.github.com>
2022-11-18 11:13:30 -0800

    switched to new input

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    //see presentation slide for details on each
     public string cardName;
     public string cardTopic;
     public string cardType;
     public string cardSubtype;
     public string cardEffect;
 
+
+    //sets all the values for the card
     internal void setParams(string name, string topic, string type, string subtype, string effect)
     {
         cardName = name;
