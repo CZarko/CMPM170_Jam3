f293f27fc64c4cbb3931d9a8bf12cf003777937a
JoeFrumenti <111709603+jfrument@users.noreply.github.com>
2022-11-21 13:20:02 -0800

    Scaled UI with screen size on date

---
+++ Assets/Scripts/CardFunctions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFunctions : MonoBehaviour
+{
+    void AddAttraction(int toAdd)
+    {
+
+    }
+}
